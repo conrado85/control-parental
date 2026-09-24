de39dac420a3f39646e5951a65f5e3412d37eaf6
conrado <carlos_ag20@hotmail.com>
2026-09-24 14:42:05 -0300

    feat(backend): add health check route

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/", (_req, res) => {
+  res.json({
+    message: "Backend funcionando correctamente",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Backend ejecutándose en http://localhost:${PORT}`);
 });
